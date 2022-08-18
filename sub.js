eb378ec59e98e3c8a039ce09f16878de2da641ad
Deepak <dlohani1@gmail.com>
2022-08-18 07:55:32 +0530

    subtract function created

---
+++ sub.js
@@ -0,0 +1,10 @@
+function sub() {
+
+var var1 = document.getElementById("firstno").value
+var var2 = document.getElementById("secondno").value
+
+var result = var1 - var2
+document.getElementById("firstno").value = result
+
+
+}
